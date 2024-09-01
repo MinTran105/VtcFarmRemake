ce1dc1d54b0873de9623307f92f79ae035090a21
Minh Tran <silversea105@gmail.com>
2024-09-01 21:44:10 +0700

    Complete-Farming-Action
    Dig, Plant, Harvest

---
+++ Assets/Scripts/PlayerAction.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PlayerAction : MonoBehaviour
+{
+    public Tilemap tm_ground;
+    public Tilemap tm_grass;
+    public Tilemap tm_farm;
+
+    public TileBase tb_ground;
+    public TileBase tb_grass;
+    public TileBase tb_farm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            TileBase currentTbGrass = GetCurrentTile(transform.position,tm_grass);
+            TileBase currentTbGround = GetCurrentTile(transform.position,tm_ground);
+            if(currentTbGround != null && currentTbGrass == tb_grass)
+            {
+                tm_grass.SetTile(TransformPosInt(), null);
+            }
+            Debug.Log("Dao"); 
+        }
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            TileBase currentTbGrass = GetCurrentTile(transform.position, tm_grass);
+            TileBase currentTbGround = GetCurrentTile(transform.position, tm_ground);
+            TileBase currentTbFarm = GetCurrentTile(transform.position, tm_farm);
+
+            if(currentTbGrass == null && currentTbGround != null && currentTbFarm == null)
+            {
+                tm_farm.SetTile(TransformPosInt(),tb_farm);
+                Debug.Log("Trong");
+            }
+        }
+        if(Input.GetKey(KeyCode.C))
+        {
+            TileBase currentTbFarm = GetCurrentTile(transform.position, tm_farm);
+            if(currentTbFarm == tb_farm)
+            {
+                tm_farm.SetTile(TransformPosInt(), null);
+                tm_grass.SetTile(TransformPosInt(), tb_grass);
+                Debug.Log("Thu Hoach");
+            }
+        }
+    }
+
+    private TileBase GetCurrentTile(Vector3 pos,Tilemap tilemap)
+    {
+        Vector3Int cellPos = tm_grass.WorldToCell(pos);
+
+        return tilemap.GetTile(cellPos) as TileBase;
+    }
+    private Vector3Int TransformPosInt()
+    {
+        return tm_grass.WorldToCell(transform.position);
+    }
+}
