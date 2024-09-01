818e153e51df751baf47d20bcfc3ee94d7b89b77
Minh Tran <silversea105@gmail.com>
2024-09-01 17:26:36 +0700

    Player-Move-By-Mouse

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,8 +6,11 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Vector2 direction;
+    private Vector2 movement;
+    private bool isMoving = false;
     private Rigidbody2D playerRb;
     private Animator playerAnim;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -19,12 +22,26 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        direction.x = Input.GetAxis("Horizontal");
-        direction.y = Input.GetAxis("Vertical");
-        direction = 
+        //direction.x = Input.GetAxis("Horizontal");
+        //direction.y = Input.GetAxis("Vertical");
+        //Move by Mouse
+        if(Input.GetMouseButtonDown(0))
+        {
+            direction = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            movement = (direction - playerRb.position).normalized;
+            isMoving = true;
+        }
+
     }
     private void FixedUpdate()
     {
-        
+        if(isMoving)
+        {
+            playerRb.MovePosition(playerRb.position + movement * speed * Time.fixedDeltaTime);
+        }
+        if(Vector2.Distance(direction, playerRb.position)  <=0.1f)
+        {
+            isMoving = false;
+        }
     }
 }
