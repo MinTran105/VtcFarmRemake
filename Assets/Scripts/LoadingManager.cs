5f51cf6ae029932fcddf902252ddc755570c94d9
Minh Tran <silversea105@gmail.com>
2024-09-05 20:28:39 +0700

    create-LoadSceneFixedTime(SmallData)

--- Assets/Scripts/LoadingManager.cs
+++ Assets/Scripts/LoadingManager.cs
@@ -11,12 +11,13 @@ public class LoadingManager : MonoBehaviour
     public static string SCENE_NAME = "Play";
     public GameObject progressBar;
     public Text textPercent;
-    private float fixedLoandingTime = 3f;
+    private float fixedLoadingTime = 3f;
 
     private void Start()
     {
-        StartCoroutine(LoadSceneAsync(SCENE_NAME));
+        StartCoroutine(LoadSceneFixedTime(SCENE_NAME));
     }
+    //cho du lieu tai k moi load scene, danh cho game du lieu lon
     IEnumerator LoadSceneAsync(string sceneName)
     {
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
@@ -27,11 +28,28 @@ public class LoadingManager : MonoBehaviour
 
             progressBar.GetComponent<Image>().fillAmount = progress;
 
-            textPercent.text = (progress * 100).ToString("0");
+            textPercent.text = (progress * 100).ToString("0") + "%";
 
             yield return null;
         }
+    }
+    //load scene sau 1 khoang thoi gian, danh cho game du lieu nho
+    IEnumerator LoadSceneFixedTime(string sceneName)
+    {
+        float elapsedTime = 0f;
 
+        while(elapsedTime < fixedLoadingTime)
+        {
+            float progress = Mathf.Clamp01(elapsedTime / fixedLoadingTime);
 
+            progressBar.GetComponent<Image>().fillAmount = progress;
+
+            textPercent.text = (progress * 100).ToString("0") + "%";
+
+            elapsedTime += Time.deltaTime;
+
+            yield return null;
+        }
+        SceneManager.LoadScene(sceneName);
     }
 }
